bdf7e417959e76926ddfe6b409bb8e15ea36fbcd
lilinnca <2022335880@ifam.edu.br>
2024-06-27 14:20:11 -0400

    Modificações (27/06)

--- app/src/main/java/com/example/tasktide/MinhaConta.java
+++ app/src/main/java/com/example/tasktide/MinhaConta.java
@@ -3,15 +3,42 @@ package com.example.tasktide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MinhaConta extends AppCompatActivity {
+
+    private TextView txtNomeUsuario;
+    private TextView txtHorasUsuario;
+    private TextView txtTipoUsuario;
+    private TextView txtEmailUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minha_conta);
+
+        txtNomeUsuario = findViewById(R.id.txtNomeUsuario);
+        txtHorasUsuario = findViewById(R.id.txtHorasUsuario);
+        txtTipoUsuario = findViewById(R.id.txtTipoUsuario);
+        txtEmailUsuario = findViewById(R.id.txtEmailUsuario);
+
+        carregarDadosUsuario();
+    }
+
+    private void carregarDadosUsuario() {
+        SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        String nome = prefs.getString("nome", "");
+        String horas = "192 horas contabilizadas"; // Exemplo
+        String tipo = "Discente - Ensino Médio"; // Exemplo
+        String email = prefs.getString("email", "");
+
+        txtNomeUsuario.setText(nome);
+        txtHorasUsuario.setText(horas);
+        txtTipoUsuario.setText(tipo);
+        txtEmailUsuario.setText(email);
     }
 
     public void telaperfil(View view) {
@@ -20,6 +47,8 @@ public class MinhaConta extends AppCompatActivity {
     }
 
     public void telameuseventos(View view) {
+        Intent in = new Intent(MinhaConta.this, MeusEventos.class);
+        startActivity(in);
     }
 
     public void telacriarevento(View view) {
